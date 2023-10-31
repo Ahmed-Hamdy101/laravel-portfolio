44f6f6a63c0fbfb530a1b428e2352b4287103027
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-10-31 20:25:42 +0000

    update vite 📈

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import laravel from 'laravel-vite-plugin'
-import {defineConfig} from 'vite'
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/js/app.js',
-        ]),
-       {
-           name: 'blade',
-           handleHotUpdate({ file, server }) {
-               if (file.endsWith('.blade.php')) {
-                   server.ws.send({
-                       type: 'full-reload',
-                       path: '*',
-                   });
-               }
-           },
-       }
+        laravel({
+            input: "resources/js/app.js",
+            refresh: true,
+        }),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
     ],
-})
+});
