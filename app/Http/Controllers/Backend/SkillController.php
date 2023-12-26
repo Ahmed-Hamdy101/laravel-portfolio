82d4b915bc9f9db5494bcf042b7219a3edc63ac1
Ahmed-Hamdy101 <forln.nostrom@yandex.com>
2023-12-26 21:30:09 +0200

    update skillsController 🈂️

--- app/Http/Controllers/Backend/SkillController.php
+++ app/Http/Controllers/Backend/SkillController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Backend;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
+use App\Http\Requests\UpdateSkillRequest;
 use App\Models\Skill;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 class SkillController extends Controller
 {
@@ -31,7 +34,7 @@ class SkillController extends Controller
     {
         // if the file it has 
         if ($request->hasFile('image')) {
-            // valid the image 
+            // valid the image  and store linking between storage and public folder
             $image = $request->file('image')->store('skills', 'public');
             // create new skills from req form
             Skill::create(
@@ -51,6 +54,35 @@ class SkillController extends Controller
     {
         return view('skills.edit', compact('skill'));
     }
+    public function update(Request $request, Skill $skill)
+    {
+        $request->validate([
+            'name' => 'min:3',
+            'image' => ['nullable', 'required']
+        ]);
+    
+        // Get the current image path
+        $currentImage = $skill->image;
+    
+        // Check if a new image is provided
+        if ($request->hasFile('image')) {
+            // Delete the current image
+            Storage::delete($currentImage);
+    
+            // Store the new image and update the image variable
+            $imagePath = $request->file('image')->store('skills', 'public');
+            $currentImage = $imagePath;
+        }
+    
+        // Update the skill
+        $skill->update([
+            'name' => $request->name,
+            'image' => $currentImage,
+        ]);
+    
+        return redirect()->route('skills.index');
+    }
+    
     public function destroy($id)
     {   Skill::destroy($id);
         return response('user delete successfully', Response::HTTP_NO_CONTENT);
